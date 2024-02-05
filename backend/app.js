cd3c88b2fc47f92544a0b31000b7a128e497cbbf
rucker122 <rucker122@users.noreply.github.com>
2024-02-05 22:59:08 +0800

    Add verify api

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3300;
+const SITE_VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify';
+const SECRET_KEY = '';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,7 +16,7 @@ app.post('/api/Submit', (req, res) => {
   const postData = req.body;
 
   console.log('Received POST data:', postData);
-
+  verifyCaptcha(req.token);
   res.send({ data: postData });
 });
 
@@ -22,3 +24,13 @@ app.post('/api/Submit', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+function verifyCaptcha(t) {
+  fetch(SITE_VERIFY_URL, {
+    method: 'POST',
+    body: JSON.stringify({
+      secret: SECRET_KEY,
+      response: t
+    })
+  }).then(res => console.log(res));
+}
